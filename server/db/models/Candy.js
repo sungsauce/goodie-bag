6e28caa32763119f72cbc6ec4a0b2c01d7598bc2
Wendy Sung <wendalssung@gmail.com>
2019-02-08 16:28:56 -0500

    added model

--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -3,4 +3,31 @@ const db = require('../database');
 
 module.exports = db.define('candy', {
   // define your model here!
+  name: {
+    type: Sequelize.STRING,
+    validate: {
+      isNull: false,
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.STRING,
+    validate: {
+      isNull: false,
+      notEmpty: true
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    validate: {
+      max: 10
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/9/9e/Peppermint_Butler.png/revision/latest?cb=20100512224052',
+    validate: {
+      isURL: true
+    }
+  }
 });
